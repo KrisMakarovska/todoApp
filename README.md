4f2418464f579d2c1d3e614ff7423bf266e0e245
Kristina Makarovska <kris.makarovska@gmail.com>
2023-03-22 22:16:48 +0200

    todo-app-1.1

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Todo App with API
+
+Technologies:
+- React,
+- Javascript,
+- Typescript,
+- HTML, 
+- CSS,  
+
+[DEMO LINK](https://<KrisMakarovska.github.io/react_todo-app-with-api/)
---
+++ src/components/Notification.tsx
@@ -0,0 +1,52 @@
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+
+type Props = {
+  errorMessage: string,
+  setErrorMessage: (e: string) => void,
+};
+
+export const Notifications: React.FC<Props> = ({
+  errorMessage,
+  setErrorMessage,
+}) => {
+  const [isHidden, setisHidden] = useState(true);
+
+  useEffect(() => {
+    const notificationCloser = () => {
+      setTimeout(() => {
+        setisHidden(true);
+        setErrorMessage('');
+      }, 3000);
+    };
+
+    if (errorMessage.length) {
+      setisHidden(false);
+      notificationCloser();
+    } else {
+      setisHidden(true);
+    }
+  }, [errorMessage]);
+
+  return (
+    <div
+      className={classNames(
+        'notification',
+        'is-danger',
+        'is-light',
+        'has-text-weight-normal', {
+          hidden: isHidden,
+        },
+      )}
+      hidden={isHidden}
+    >
+      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      <button
+        type="button"
+        className="delete"
+        onClick={() => setisHidden(true)}
+      />
+      {errorMessage}
+    </div>
+  );
+};
